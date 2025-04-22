bb82f6adb07f364072d9f295da3c8b74c22cf308
신현식 <hunsik777@gmail.com>
2025-04-22 17:44:23 +0900

    login 작업

---
+++ get.mjs
@@ -0,0 +1,21 @@
+import express from "express";
+import fs from "fs";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.get("/login", (req, res) => {
+  fs.readFile("login.html", (err, data) => {
+    if (err) {
+      res.status(500);
+      return res.send("읽기 오류");
+    }
+    res.status(200, { "Content-Type": "text/html" });
+    res.send(data);
+  });
+});
+
+app.listen(3000, () => {
+  console.log("서버 실행 중");
+});
